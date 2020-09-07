6af4a6a6940c3fd8ced64d4c5205b509da52da00
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2020-09-07 17:25:59 +0300

    List implemented

--- Data Structures Fundamentals/01.Linear Data Structures/Problem01.List/List.cs
+++ Data Structures Fundamentals/01.Linear Data Structures/Problem01.List/List.cs
@@ -9,24 +9,31 @@
         private const int DEFAULT_CAPACITY = 4;
         private T[] _items;
 
-        public List()
-            : this(DEFAULT_CAPACITY) {
-        }
+        //public List()
+        //    : this(DEFAULT_CAPACITY) {
+        //}
 
-        public List(int capacity)
+        public List(int capacity = DEFAULT_CAPACITY)
         {
-            throw new NotImplementedException();
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+            }
+
+            this._items = new T[capacity];
         }
 
         public T this[int index]
         {
             get
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+                return this._items[index];
             }
             set
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+                this._items[index] = value;
             }
         }
 
@@ -34,41 +41,109 @@
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            EnsureNotEmpty();
+            this._items[this.Count++] = item;
         }
+
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            return this.IndexOf(item) != -1;
         }
 
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < this._items.Length; i++)
+            {
+                if (this._items[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            ValidateIndex(index);
+            this.EnsureNotEmpty();
+
+            for (int i = this.Count; i > index; i--)
+            {
+                this._items[i] = this._items[i - 1];
+            }
+
+            this._items[index] = item;
+
+            this.Count++;
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            int indexOfElement = this.IndexOf(item);
+
+            if (indexOfElement == -1)
+            {
+                return false;
+            }
+
+            this.RemoveAt(indexOfElement);
+
+            return true;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            this.ValidateIndex(index);
+
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this._items[i] = this._items[i + 1];
+            }
+
+            this._items[this.Count - 1] = default;
+
+            this.Count--;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this._items[i];
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator() 
-            => throw new NotImplementedException();
+            => this.GetEnumerator();
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Count == this._items.Length)
+            {
+                ResizeArray();
+            }
+        }
+
+        private void ResizeArray()
+        {
+            var newArr = new T[this._items.Length * 2];
+
+            for (int i = 0; i < this._items.Length; i++)
+            {
+                newArr[i] = this._items[i];
+            }
+
+            this._items = newArr;
+        }
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException(nameof(index));
+            }
+        }
     }
 }
