0757826bd09f77daa964764b0ce354d18f76c18a
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2020-09-10 19:21:03 +0300

    Faster Queue Implemented
    :+1:

---
+++ Data Structures Fundamentals/02.Exercise Linear Data Structures/01.FasterQueue/FastQueue.cs
@@ -0,0 +1,105 @@
+﻿namespace Problem01.FasterQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class FastQueue<T> : IAbstractQueue<T>
+    {
+        private Node<T> _head;
+        private Node<T> _tail;
+        public int Count { get; private set; }
+
+        public FastQueue()
+        {
+            this._head = null;
+            this._tail = null;
+            this.Count = 0;
+        }
+        public FastQueue(Node<T> head)
+        {
+            this._head = this._tail = head;
+            this.Count = 1;
+        }
+        public bool Contains(T item)
+        {
+            var current = this._head;
+
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public T Dequeue()
+        {
+            this.EnsureNotEmpty();
+
+            var current = this._head;
+            if (this.Count == 1)
+            {
+                this._head = this._tail = null;
+            }
+            else
+            {
+                this._head = this._head.Next;
+            }
+
+            this.Count--;
+            return current.Item;
+        }
+
+        public void Enqueue(T item)
+        {
+            var toInsert = new Node<T>
+            {
+                Item = item,
+            };
+
+            if (this.Count == 0)
+            {
+                this._head = this._tail = toInsert;
+            }
+            else
+            {
+                this._tail.Next = toInsert;
+                this._tail = toInsert;
+            }
+
+            this.Count++;
+        }
+
+        public T Peek()
+        {
+            this.EnsureNotEmpty();
+
+            return this._head.Item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = this._head;
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Count == 0)
+                throw new InvalidOperationException();
+        }
+    }
+}
