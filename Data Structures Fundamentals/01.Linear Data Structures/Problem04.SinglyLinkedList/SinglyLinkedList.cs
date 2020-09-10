354722cd1c96342ab75084122a18a887248a2c18
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2020-09-10 19:00:27 +0300

    LinkedList implemented

--- Data Structures Fundamentals/01.Linear Data Structures/Problem04.SinglyLinkedList/SinglyLinkedList.cs
+++ Data Structures Fundamentals/01.Linear Data Structures/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -8,44 +8,128 @@
     {
         private Node<T> _head;
 
+        public SinglyLinkedList()
+        {
+            this._head = null;
+            this.Count = 0;
+        }
+
+        public SinglyLinkedList(Node<T> head)
+        {
+            this._head = head;
+            this.Count = 1;
+        }
+
         public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node<T>
+            {
+                Value = item,
+                Next = this._head
+            };
+
+            this._head = newNode;
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node<T>
+            {
+                Value = item
+            };
+
+            if (this._head is null)
+                this._head = newNode;
+            else
+            {
+                var current = this._head;
+
+                while (current.Next != null)
+                    current = current.Next;
+
+                current.Next = newNode;
+            }
+
+            this.Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            this.ValidateIfEmpty();
+
+            return this._head.Value;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            this.ValidateIfEmpty();
+            Node<T> current = this._head;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            return current.Value;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            this.ValidateIfEmpty();
+
+            var headItem = this._head.Value;
+            var newHead = this._head.Next;
+            this._head.Next = null;
+            this._head = newHead;
+            this.Count--;
+
+            return headItem;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            this.ValidateIfEmpty();
+
+            if (this._head.Next is null)
+                return this.RemoveFirst();
+
+            var current = this._head;
+
+            while (current.Next.Next != null)
+            {
+                current = current.Next;
+            }
+
+            var lastItem = current.Next.Value;
+            current.Next = null;
+            this.Count--;
+
+            return lastItem;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            Node<T> current = this._head;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
             => this.GetEnumerator();
+
+        private void ValidateIfEmpty()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Linked List is empty");
+            }
+        }
     }
 }
