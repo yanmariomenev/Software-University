e2ebc56fda7ade17eb876de0b1bbedd5a8b3a8ee
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2020-10-01 14:24:34 +0300

    initial create

---
+++ Angular Fundamentals/forms-exercise/forms/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
