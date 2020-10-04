93f24bd1cf59b4c4adc9cf2fb715e405d40c73d5
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2020-10-04 16:41:39 +0300

    template form done

--- Angular Fundamentals/forms-exercise/forms/src/app/app.module.ts
+++ Angular Fundamentals/forms-exercise/forms/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { FormsModule } from '@angular/forms';
+import { ImageUrlValidatorDirective } from './image-url-validator.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterFormComponent
+    RegisterFormComponent,
+    ImageUrlValidatorDirective
   ],
   imports: [
     BrowserModule,
